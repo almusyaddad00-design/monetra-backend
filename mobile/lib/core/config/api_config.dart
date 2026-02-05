1329642e1b11559a8e79ab96e9d5b214670f6866
Alfan Musyaddad <almusyaddad00@gmail.com>
2026-02-05 16:16:48 +0700

    Increase API timeout to 30s for Railway cold starts

--- mobile/lib/core/config/api_config.dart
+++ mobile/lib/core/config/api_config.dart
@@ -27,6 +27,6 @@ class ApiConfig {
     return 'http://10.0.2.2:8000/api';
   }
 
-  static const int connectTimeout = 15000;
-  static const int receiveTimeout = 15000;
+  static const int connectTimeout = 30000;
+  static const int receiveTimeout = 30000;
 }
